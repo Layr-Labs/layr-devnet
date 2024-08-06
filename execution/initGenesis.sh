12ef5889a2cc2cf7131b09ada326752d5a701873
Sean McGary <sean@seanmcgary.com>
2024-08-06 11:55:51 -0500

    Add convenience script to init genesis

---
+++ execution/initGenesis.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+OVERWRITE="${OVERWRITE:-false}"
+CONFIG_FILE="${CONFIG_FILE:-/consensus/config.yml}"
+GENESIS_INPUT="${GENESIS_INPUT:-/execution/genesis.json}"
+GENESIS_OUTPUT="${GENESIS_OUTPUT:-/execution/genesis.json}"
+OUTPUT_SSZ="${OUTPUT_SSZ:-/consensus/genesis.ssz}"
+
+if [[ -f $GENESIS_OUTPUT && $OVERWRITE == "false" ]]; then
+    echo "Genesis file already exists. Set OVERWRITE=true to overwrite."
+    exit 0
+fi
+
+rm $GENESIS_OUTPUT $OUTPUT_SSZ
+
+prysmctl testnet generate-genesis \
+    --fork=capella \
+    --num-validators=64 \
+    --genesis-time-delay=15 \
+    --output-ssz=$OUTPUT_SSZ \
+    --chain-config-file=$CONFIG_FILE \
+    --geth-genesis-json-in=$GENESIS_INPUT \
+    --geth-genesis-json-out=$GENESIS_OUTPUT
